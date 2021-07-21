14e6788a6178eeedbe1d12ffdddf0dac273aabdd
Tamirshazman <A0547799597t>
2021-07-21 09:54:04 +0300

    change countries

--- dataStreamining.py
+++ dataStreamining.py
@@ -1,62 +1,101 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 from pyspark.sql.types import *
-from pyspark.sql.functions import to_date, col, month, year
+from pyspark.sql.functions import to_date, col, month, year, mean
 
-database_name = "tmyr"
-url = 'jdbc:sqlserver://technionddscourse.database.windows.net:1433;database=tmyr;'
+"""
+In this script, the data we'll be downloaded and modify for insertion to DB. The countries that we selected to download
+their data are England, Germany, France, Spain and Italy.First of all the script fetch Station table from the DB 
+(table that already been inserted to DB with information about station location).After transform the data from json file 
+structure to Spark DF and cleans the the data from outliers (Q_FLAG).Then it's filter all the old data 
+(older then 1.1.2000) and the keeps only samples from the core variables. After that it's aggregates the sample from
+each day to one day (by using the max value of samples from the day), and then aggregate the mean (of each year) of each
+month. Finally it's insert each county to it Table and calculates statistics of each station (the mean of each variable
+for each month for the whole period that inserted to DB.
+"""
 
-table_name = "test"
+# information for connection
+kafka_server = 'dds2020s-kafka.eastus.cloudapp.azure.com:9092'
 username = "tmyr"
 password = "Qwerty12!"
+server_name = "jdbc:sqlserver://technionddscourse.database.windows.net:1433"
+database_name = "tmyr"
+url = server_name + ";" + "databaseName=" + database_name + ";"
+# core variable names
 cor_var = ["PRCP", "SNOW", "SNWD", "TMAX", "TMIN"]
+# countries and their FIPS code
+my_countries = [('GB', 'England'), ('GM', 'Germany'), ('FR', 'France'), ('SP', 'Spain'), ('IT', 'Italy')]
+# main DF scheme
+noaa_schema = StructType([StructField('StationId', StringType(), False),
+                          StructField('Date', StringType(), False),
+                          StructField('Variable', StringType(), False),
+                          StructField('Value', IntegerType(), False),
+                          StructField('M_Flag', StringType(), True),
+                          StructField('Q_Flag', StringType(), True),
+                          StructField('S_Flag', StringType(), True),
+                          StructField('ObsTime', StringType(), True)])
 
 if __name__ == "__main__":
     spark = SparkSession.builder.getOrCreate()
-    kafka_server = 'dds2020s-kafka.eastus.cloudapp.azure.com:9092'
-    noaa_schema = StructType([StructField('StationId', StringType(), False),
-                              StructField('Date', StringType(), False),
-                              StructField('Variable', StringType(), False),
-                              StructField('Value', IntegerType(), False),
-                              StructField('M_Flag', StringType(), True),
-                              StructField('Q_Flag', StringType(), True),
-                              StructField('S_Flag', StringType(), True),
-                              StructField('ObsTime', StringType(), True)])
+    # fetch the station information
+    StationDF = spark.read \
+        .format("com.microsoft.sqlserver.jdbc.spark") \
+        .option("url", url) \
+        .option("dbtable", "Stations") \
+        .option("user", username) \
+        .option("password", password).load() \
+        .drop("state")
+    StationDF.show(20)
 
+    # download from kafka the data and modify it.
     df = spark \
-        .read \
+        .readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", kafka_server) \
-        .option("subscribe", "ES") \
+        .option("subscribe", "GB, GM, FR, SP, IT") \
         .load() \
         .selectExpr("CAST(value AS STRING)") \
-        .select(F.from_json(F.col("value"), schema=noaa_schema).alias('json')) \
+        .select(F.from_json(col("value"), schema=noaa_schema).alias('json')) \
         .select("json.*") \
+        .filter("CAST(Date AS INT) > 20000101") \
         .filter(~col("Q_Flag").isNull() | col("Variable").isin(cor_var) == True) \
         .withColumn("Date", to_date(col("date"), 'yyyyMMdd')) \
         .groupby("StationId", "Date", "Variable").max("Value") \
-        .groupby("StationId", year("Date").alias("Year"), month("Date").alias("Month"), "Variable").mean("max(Value)").\
-        alias("Mean")
+        .groupby("StationId", year("Date").alias("Year"), month("Date").alias("Month"), "Variable"). \
+        agg(mean("max(Value)").alias("Mean"))
     df.show(20)
-    dfUSA = df.filter(col("Sta") == "PRCP").drop("Variable").withColumnRenamed("max(Value)", "PRCP")
-    #dfSNOW = df.filter(col("Variable") == "SNOW").drop("Variable").withColumnRenamed("max(Value)", "SNOW")
-    #dfSNWD = df.filter(col("Variable") == "SNWD").drop("Variable").withColumnRenamed("max(Value)", "SNWD")
-    #dfTMAX = df.filter(col("Variable") == "TMAX").drop("Variable").withColumnRenamed("max(Value)", "TMAX")
-    #dfTMIN = df.filter(col("Variable") == "TMIN").drop("Variable").withColumnRenamed("max(Value)", "TMIN")
-    #dfFinal = dfPRCP.join(dfSNOW, on=[((dfPRCP.StationId == dfSNOW.StationId) & (dfPRCP.Date == dfSNOW.Date))],
-
-    #                    how='leftsemi').join()
-    #dfFinal.show(20)
-    """
-    try:
-        df.write \
-            .format("com.microsoft.sqlserver.jdbc.spark") \
-            .mode("overwrite") \
-            .option("url", url) \
-            .option("dbtable", "test") \
-            .option("user", "tmyr") \
-            .option("password", "Qwerty12!") \
-            .save()
-    except ValueError as error:
-        print("Connector write failed", error)
-    """
+    # loops on each county and takes it's statistic
+    for fipsCode, countyName in my_countries:
+        # filter the current county
+        finalDf = df.filter(f"StationId LIKE '{fipsCode}%'")
+        # create a DF for statistics information
+        statDf = finalDf.drop("Year", "Month", "Mean").distinct().join(StationDF, on="StationId", how='inner')
+        # agg for each month
+        temp = finalDf.groupby("StationId", "Month").agg(mean("Mean").alias("Mean"))
+        # loops on month
+        for month in range(1, 13):
+            monthDf = temp.filter(col("Month") == month)
+            statDf = statDf.join(monthDf, on=((statDf.StationId == monthDf.StationId) &
+                                              (statDf.Variable == monthDf.Variable)), how='leftouter')
+            statDf = statDf.withColumnRenamed("Mean", f"Mean of {str(month)}")
+        try:
+            # insert county table
+            finalDf.write \
+                .format("com.microsoft.sqlserver.jdbc.spark") \
+                .mode("overwrite") \
+                .option("url", url) \
+                .option("dbtable", countyName + "_table") \
+                .option("user", username) \
+                .option("password", password) \
+                .save()
+            # append to statistic table
+            finalDf.write \
+                .format("com.microsoft.sqlserver.jdbc.spark") \
+                .mode("append") \
+                .option("url", url) \
+                .option("dbtable", "StationStat") \
+                .option("user", username) \
+                .option("password", password) \
+                .save()
+        except ValueError as error:
+            print("Connector write failed", error)
